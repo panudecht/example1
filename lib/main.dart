0e87d4269e9c1b2f66a4c762624027aaac4cf618
panudecht <panudech@gmail.com>
2024-05-16 14:57:23 +0700

    update2

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 29, 77, 234)),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 29, 77, 234)),
         useMaterial3: true,
       ),
       home: const MyHomePage(),
--- lib/screen/my_home_page.dart
+++ lib/screen/my_home_page.dart
@@ -6,6 +6,14 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Hello World");
+    return Scaffold(
+      drawer: const Drawer(
+        child: Text("My drawer"),
+      ),
+      appBar: AppBar(title: const Text("My Home Page")),
+      body: const Center(
+        child: Text("Hello world", style: TextStyle(fontSize: 30, color: Color.fromARGB(255, 18, 132, 188)),),
+      ),
+    );
   }
 }
